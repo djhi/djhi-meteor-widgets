e1339c64d4515627c65b150edc24612d797b773a
djhi <gildas.garcia@gmail.com>
2014-07-09 10:26:18 +0200

    initial commit

---
+++ package.js
@@ -0,0 +1,27 @@
+Package.describe({
+  summary: "Widgets for autoform and bootstrap 3. Not meant to be used by anyone but me :)"
+});
+
+Package.on_use(function (api, where) {
+  api.use(['moment-with-langs', 'numeral', 'bower', 'ui'], ['client', 'server']));
+  api.imply(['moment-with-langs', 'numeral', 'bower'], ['client', 'server']);
+
+  api.use(['clockpicker'], ['client']);
+  api.imply(['clockpicker'], ['client']);
+  
+  api.add_files(['lib/checkboxButton.css', 'lib/checkboxButton.html', 'lib/checkboxButton.js'], ['client']);
+  api.add_files(['lib/date.html', 'lib/date.js'], ['client']);
+  api.add_files(['lib/email.html', 'lib/email.js'], ['client']);
+  api.add_files(['lib/mobile.html', 'lib/mobile.js'], ['client']);
+  api.add_files(['lib/phone.html', 'lib/phone.js'], ['client']);
+  api.add_files(['lib/radiosButtons.html', 'lib/radiosButtons.js'], ['client']);
+  api.add_files(['lib/tags.html', 'lib/tags.js'], ['client']);
+  api.add_files(['lib/time.html', 'lib/time.js'], ['client']);
+  api.add_files(['lib/nolabel.html'], ['client']);
+});
+
+Package.on_test(function (api) {
+  api.use(['moment-with-langs', 'numeral', 'meteor-formatting', 'tinytest', 'test-helpers']);
+
+  api.add_files('meteor-formatting_tests.js', ['client', 'server']);
+});
